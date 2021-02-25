738c0aaa6d3dd85c3d4168e0a16d2a2921627b79
Andrew <Klmn>
2021-02-25 17:12:36 +0200

    Add: module structure to js app

--- html-css-cinema-booking/src/module/ticket-updater.js
+++ html-css-cinema-booking/src/module/ticket-updater.js
@@ -1,45 +1,17 @@
-const mainScreen = document.querySelector('main');
-const selectorForm = document.querySelector('.booking_form');
-const draftTicket = document.querySelector('.ticket');
-const initialTicketView = draftTicket.innerHTML;
-const SEAT_PRICE_IN_USD = 20;
-const bookButton = selectorForm.querySelector('.book_button');
-
-const confirmationScreen = document.querySelector('.confirm_screen');
-const ticketDateTime = document.querySelector('.ticket_date_time');
-const ticketSeats = document.querySelector('.ticket_seats');
-const ticketTotal = document.querySelector('.ticket_total');
-const cancelButton = document.querySelector('.cancel_confirm_button');
-const payButton = document.querySelector('.confirm_button');
+import {
+  selectorForm,
+  draftTicket,
+  initialTicketView,
+  SEAT_PRICE_IN_USD,
+  ticketDateTime,
+  ticketSeats,
+  ticketTotal,
+} from './config';
 
 let lastSelectedDate = selectorForm.querySelector('.date_selector > .radio_control:checked').getAttribute('aria-label');
 let lastSelectedTime = selectorForm.querySelector('.time_selector > .radio_control:checked').getAttribute('aria-label');
 
-const showConfirmScreen = () => {
-  mainScreen.classList.add('hidden');
-  confirmationScreen.classList.remove('hidden');
-  cancelButton.focus();
-};
-
-const hideConfirmScreen = () => {
-  confirmationScreen.classList.add('hidden');
-  mainScreen.classList.remove('hidden');
-  bookButton.focus();
-};
-
-const payButtonListener = () => {
-  alert('Action disabled!');
-};
-
-const bookButtonListener = (event) => {
-  const selectedSeats = selectorForm.querySelectorAll('.seat_control:checked');
-  if (selectedSeats.length > 0) {
-    showConfirmScreen();
-  }
-  event.preventDefault();
-};
-
-const updateTicket = () => {
+export default function updateTicket() {
   const selectedSeats = [...selectorForm.querySelectorAll('.seat_control:checked')];
 
   if (selectedSeats.length === 0) {
@@ -83,10 +55,3 @@ const updateTicket = () => {
   }).join('')}`;
   ticketTotal.innerHTML = `Price: $${total.toFixed(2)}`;
 };
-
-document.addEventListener('DOMContentLoaded', () => {
-  selectorForm.addEventListener('change', updateTicket);
-  bookButton.addEventListener('click', bookButtonListener);
-  cancelButton.addEventListener('click', hideConfirmScreen);
-  payButton.addEventListener('click', payButtonListener);
-});
