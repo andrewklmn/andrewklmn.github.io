133a007ea617b24a19744343ba84c241fd343f27
Andrew <Klmn>
2021-04-14 11:28:43 +0300

    Add: deselect seats by cancel button

--- html-css-cinema-booking/src/index.js
+++ html-css-cinema-booking/src/index.js
@@ -6,18 +6,20 @@ import {
   payButton,
 } from './modules/config';
 
-/*
-import './css/style.css';
-import './css/ticket_view.css';
-*/
-
 import showScreen from './modules/screen-show';
-import { payButtonListener, bookButtonListener } from './modules/button-listeners';
+
+import {
+  payButtonListener,
+  bookButtonListener,
+  cancelButtonListener,
+} from './modules/button-listeners';
+
 import updateTicket from './modules/ticket-updater';
 
 document.addEventListener('DOMContentLoaded', () => {
   selectorForm.addEventListener('change', updateTicket);
   bookButton.addEventListener('click', bookButtonListener);
+  cancelButton.addEventListener('click', cancelButtonListener);
   cancelButton.addEventListener('click', () => showScreen(screens.main, bookButton));
   payButton.addEventListener('click', payButtonListener);
 });
